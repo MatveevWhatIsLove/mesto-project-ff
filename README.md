80f6192dd20493fb560e0ffc342a7239d9964fd8
Matveev Vladislav <vl4dyslav.matveev@yandex.ru>
2024-10-15 22:34:21 +0300

    com 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/MatveevWhatIsLove/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,30 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+let placesList = document.querySelector('.places__list');
+let cardTemplate = document.querySelector('#card-template').content;
+
+let cardDelete = () => (e) =>{
+    let parentCard = e.target.closest('.card');
+    parentCard.remove();
+}
+
+function cardRender(item){
+    let card = cardTemplate.querySelector('.card').cloneNode(true);
+    let cardImage = card.querySelector('.card__image');
+    let cardTitle = card.querySelector('.card__title');
+    let cardDeleteButton = card.querySelector('.card__delete-button');
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardTitle.textContent = item.name;
+
+    cardDeleteButton.addEventListener('click', cardDelete(true));
+
+    console.log(card)
+    return card
+}
+
+initialCards.forEach(function(item){
+    placesList.append(cardRender(item));
+})
